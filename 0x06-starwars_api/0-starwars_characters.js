3088a44ecf48b702a9b479e48ae14a8bdc746366
GideonBature <infoaboutgideon@gmail.com>
2024-04-17 07:31:38 +0100

    fix code to use recursion

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
+const movieURL = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-function getAPIRequest (url) {
-  return new Promise((resolve, reject) => {
-    request(url, { json: true }, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
-      }
-    });
+function getMovieCharacters (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], { json: true }, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log(body.name);
+    getMovieCharacters(characterList, index + 1);
   });
 }
 
-async function fetchAPIData (movieId) {
-  try {
-    const filmData = await getAPIRequest(`https://swapi-api.alx-tools.com/api/films/${movieId}/`);
-    const charactersURL = filmData.characters;
-    charactersURL.forEach(async (url) => {
-      const characterData = await getAPIRequest(url);
-      console.log(characterData.name);
-    })
-  } catch (error) {
+request(movieURL, { json: true }, (error, response, body) => {
+  if (error) {
     console.error(error);
   }
-}
-
-fetchAPIData(movieId);
+  const characterList = body.characters;
+  getMovieCharacters(characterList, 0);
+});
