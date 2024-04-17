996ccf606e845e52336ab776896ce75c90b5d8c9
GideonBature <infoaboutgideon@gmail.com>
2024-04-17 03:32:06 +0100

    a script that prints all characters of a Star Wars movie:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,2 +1,31 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
 
+function makeAPIRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+
+async function fetchAPIData () {
+  try {
+    const filmData = await makeAPIRequest(`https://swapi-api.alx-tools.com/api/films/${id}/`);
+    const charactersURL = filmData.characters;
+    charactersURL.map(async (characterURL) => {
+      const characterData = await makeAPIRequest(characterURL);
+      const characterName = characterData.name;
+      console.log(characterName);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchAPIData();
