bc6dae9ec74f6dc83ebca7e441ab1ce8653d3d0b
GideonBature <infoaboutgideon@gmail.com>
2024-04-25 13:16:33 +0100

    fix pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,13 +2,14 @@
 """0. Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """Given an n x n 2D matrix, rotate it
     90 degrees clockwise
-    
+
     @Args:
         matrix (nested list): The 2D Matrix
-    
+
     @Returns:
         matrix (nested list): The rotated 2D Matrix
     """
